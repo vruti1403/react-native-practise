83e25f0231f92a1ca6e9282cb9bf9dd32809c21b
vruti1403 <132568469+vruti1403@users.noreply.github.com>
2023-05-04 17:25:02 +0530

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,12 @@
+import {Text,View} from 'react-native';
+import React from 'react';
+const Data = () =>{
+   
+    return (
+      <View >
+        <Text style={{fontSize:25}}>name:vruti </Text>
+        <Text style={{fontSize:25}}>age : 20 </Text>
+      </View>
+    );
+    }
+  export default Data;
