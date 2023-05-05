8fcd9f0fc02c9d3c735842db752f787acc66979a
vruti1403 <132568469+vruti1403@users.noreply.github.com>
2023-05-05 17:25:29 +0530

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,71 +1,137 @@
 
-import React , {useState} from 'react';
-import Data from './components/Data';
+import React , {useState}from 'react';
+//import Data from './components/Data';
+//import Image from './Image';
+//import Form from './Form';
+//import Changesurname from './Changesurname';
+//import Welcome from './components/Welcome';''
+//import User from './components/User';
+//import Showhide from './components/showhide';
 import {
-  
-  Button,
   Text,
-  TextInput,
   View,
+  StyleSheet,
+  Button,
+  TextInput
 } from 'react-native';
-const press=()=>{
-  console.warn('event performed');
- 
-}
-const app = () =>{
-  const[surname,setsurname]=useState("shah")
-  function testname(){
-    setsurname("doshi")
-  }
-  return (
-    <View >
-      <Text style={{fontSize:35 ,color :'red'}} >
-        hello my first project app
-      </Text>
-      <Button title='press here' color={'green'} onPress={press}></Button>
-      < Data />
-      <Text style={{fontSize:25}} >surname:{surname}</Text>
-      <Button title='change surname'  onPress={testname}></Button>
-     <Form/>
-    </View>
-  );
-  }
-  const Form = () =>{
-    const[email,setEmail]=useState("");
-    const[password,setPassword]=useState("");
-    const[display,setDisplay]=useState(false);
 
-    const resetform=()=>{
-      setDisplay(false);
-      setEmail("");
-      setPassword("");
-    }
-    return(
+
+const App=()=>{
+  const[show,setshow]=useState(false);
+ 
+  return(
+ <View style={styles.view}>
+  <Text style={styles.text}>Form using show/hide</Text>
+  <View style={styles.view1}>
+  <Button title='show' onPress={() =>setshow(true)}/>
+  </View>
+  <View style={styles.view1}>
+  <Button title='hide' onPress={() =>setshow(false)}/>
+  </View>
+  {
+    show ? < User /> : null
+  }
+ </View>
+  )
+}
+const User=()=>{
+  const[name,setname]=useState("");
+  const[email,setemail]=useState("");
+  const[phone,setphone]=useState("");
+  const[address,setaddress]=useState("");
+  const[display,setDisplay]=useState(false);
+
+  const resetform=()=>{
+    setDisplay(false);
+    setemail("");
+    setname("");
+    setphone("");
+    setaddress("");
+  }
+  return(
+    <View>
+      
       <View>
-        <Text>LOGIN FORM</Text>
-        <TextInput placeholder='enter your email id' onChangeText={(text)=>setEmail(text)} value={email}/>
-        <TextInput placeholder='enter your password' secureTextEntry={true} onChangeText={(text)=>setPassword(text)} value={password}/>
-        <View>
-          <Button color={'green'} title='print details' onPress={()=>setDisplay(true)}/>
+        <Text style={styles.text1}>REGISTRATION FORM</Text>
+        <TextInput style={styles.textinput} placeholder='enter your name' onChangeText={(text)=>setname(text)} value={name}/>
+        <TextInput style={styles.textinput} placeholder='enter your email' onChangeText={(text)=>setemail(text)} value={email}/>
+        <TextInput style={styles.textinput} placeholder='enter your phone number' onChangeText={(text)=>setphone(text)} value={phone} />
+        <TextInput style={styles.textinput} placeholder='enter your address' onChangeText={(text)=>setaddress(text)} value={address}/>
+        <View style={styles.view1}>
+          <Button  title='print details' onPress={()=>setDisplay(true)}/>
+          </View>
+          <View style={styles.view1}>
           <Button  title='clear details' onPress={resetform}/>
         </View>
         
-        <View>
+        <View >
           {
             display?
-            <View>
-              <Text>email id : {email}</Text>
-              <Text>password : {password}</Text>
+            <View >
+              <Text style={styles.displayview}>name of user: {name}</Text>
+              <Text style={styles.displayview}>email id : {email}</Text>
+              <Text style={styles.displayview}>phone number : {phone}</Text>
+              <Text style={styles.displayview}>address : {address}</Text>
               </View> :null
           }
         </View>
       </View>
-      
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+  view:{
+    flex:1,
+    backgroundColor:'yellowgreen',
+    margin:10,
+    padding:30,
 
-    );
+  },
+  text:
+  {
+    textAlign:'center',
+    color:'white',
+    fontSize:25,
+    margin:10
+   
+  },
+  text1:
+  {
+    textAlign:'center',
+    color:'darkblue',
+    fontFamily:'serif',
+    fontSize:20,
+    marginTop:15,
+    marginBottom:15
+   
+  },
+  view1:{
+    backgroundColor:'black',
+    padding:15,
+  },
+  textinput:
+  {
+    borderWidth:1,
+    borderRadius:9,
+    margin:2,
+    color:'black',
+    fontSize:15
+   
+  },
+  formview:{
+    margin:10,
+    backgroundColor:'#fff',
+    borderColor:'black',
+    flexBasis:170,
+        justifyContent:'center',
+        padding:20
+  },
+  displayview:{
+    color:'white',
+    fontSize:20,
+    fontFamily:'serif'
   }
-  
+})
 
-
-export default app;
+export default App;
  
